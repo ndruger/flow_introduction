bde929b241ed0c86c5006e817cba47090c586448
ndruger <snowfullnight@gmail.com>
2016-12-20 03:06:30 +0900

    Fix

--- src/example2.js
+++ src/example2.js
@@ -2,7 +2,7 @@
 // 返り値の例
 
 function f1(): string {
-  return Math.random() < 0.5 ? 'test': null;  // nullの場合があるでのエラー
+  return Math.random() < 0.5 ? 'test': null;  // nullの場合があるでのエラー。返り値を string|null か ?string にする。後者はundefinedを許容する。
 }
 
 f1();
